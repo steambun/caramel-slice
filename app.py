c8ff3b9a14ef8c6e800a6ee9c82a8690c63b2d66
Z8bra <zanedewar@gmail.com>
2022-08-01 18:15:14 +1200

    creating first html post for guessing word

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 from quaddle_engine import chooseRandomWord
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
     random_word = chooseRandomWord()
+
+    if request.method== 'POST':
+        guess = request.form.get('guess')
+        print('This is your guess "'+guess+"'")
+
     return render_template('index.html',random_word=random_word)
